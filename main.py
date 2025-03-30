b37bc7bf537f5421145ba52cf5c22a85d13bdf82
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-03-30 01:24:38 -0300

    refactor: Passando a usar parâmetros no código

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ word_list = [
    "MySQL",
 ]
 
-def bigger_word():
+def bigger_word(word_list):
    bigger = word_list[0]
    for word in word_list:
       if len(word) > len(bigger):
          bigger = word
    return bigger
 
-def minor_word():
+def minor_word(word_list):
    minor = word_list[0]
    for word in word_list:
       if len(word) < len(minor):
          minor = word
    return minor
 
-print(f"\nA maior palavra da lista é: {bigger_word()}")
-print(f"\nA menor palavra da lista é: {minor_word()}")
+print(f"\nA maior palavra da lista é: {bigger_word(word_list)}")
+print(f"\nA menor palavra da lista é: {minor_word(word_list)}")
