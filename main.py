9df0591470972934da4892d1bec4f89c37d34ce9
Alan Fabrício <alanfabriciobarbosa1@gmail.com>
2025-04-04 22:28:38 -0300

    refactor: Atualizando o código com o que foi apren

--- main.py
+++ main.py
@@ -10,24 +10,33 @@ word_list = [
    "MySQL",
 ]
 
-def bigger_word(word_list):
-
+def bigger_word(word_list: list):
    if not word_list:
       return "Lista vazia" 
-      
+
+   if len(word_list) == 1:
+      return word_list[0]
+
    bigger = word_list[0]
-   for word in word_list:
+
+   for word in word_list[1:]:
       if len(word) > len(bigger):
          bigger = word
    return bigger
 
-def minor_word(word_list):
-
+def minor_word(word_list: list):
    if not word_list:
       return "Lista vazia"
-      
+
+   if type(word_list) != word_list:
+      return Exception('Words is not a list')
+
+   if len(word_list) == 1:
+      return word_list[0]
+
    minor = word_list[0]
-   for word in word_list:
+
+   for word in word_list[1:]:
       if len(word) < len(minor):
          minor = word
    return minor
